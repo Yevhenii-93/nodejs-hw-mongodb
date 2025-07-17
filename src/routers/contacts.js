0abaf6299f8a5bd5e9b01c51fd16219c359c6ca4
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-07-18 02:14:51 +0300

    create validateBody and contacts schema

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,16 +10,27 @@ import {
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { contactSchema, contactUpdateSchema } from '../validation/contact.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContactController));
 router.get('/:id', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post('/', ctrlWrapper(createContactController));
+router.post(
+  '/',
+  validateBody(contactSchema),
+  ctrlWrapper(createContactController),
+);
 
 router.delete('/:id', isValidId, ctrlWrapper(deleteContactController));
 
-router.patch('/:id', isValidId, ctrlWrapper(patchContactController));
+router.patch(
+  '/:id',
+  isValidId,
+  validateBody(contactUpdateSchema),
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
