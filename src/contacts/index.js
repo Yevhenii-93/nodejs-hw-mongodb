13860390390498d20c7a23a000f91f07e1a2f2e2
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-08-06 17:04:52 +0300

    fixed

--- src/contacts/index.js
+++ src/contacts/index.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 
 export const TEMP_UPLOAD_DIR = path.resolve('src', 'temp');
-export const UPLOAD_DIR = path.resolve('src', 'uploads');
+export const UPLOAD_DIR = path.resolve('src', 'uploads', 'photo');
 
 export const SMTP = {
   SMTP_HOST: 'SMTP_HOST',
